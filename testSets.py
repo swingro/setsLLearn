9936d639324c4b82f5cba540fbda0bf9ecd4a4dc
Robert Gabriel Aghenitei <robert.aghenitei@gmail.com>
2024-02-24 09:38:22 +0200

    Set Exercises save

--- testSets.py
+++ testSets.py
@@ -1,8 +1,25 @@
-set1 = {1, 2, 3, 4, 5, 6}
-set2 = {6 , 7, 8, 9, 10}
+# # set1 = {1, 2, 3, 4, 5, 6}
+# # set2 = {6 , 7, 8, 9, 10}
 
-print(set1 | set2)
+# # print(set1 | set2)
 
-print(set1 & set2)
+# # print(set1 & set2)
 
-print(set1 - set2)
+# # print(set1 - set2)
+
+# # print(set1 ^ set2)
+
+# # print(set1.union(set2))
+
+
+# a = set("abracadabra")
+# b = set("didodendron")
+# print(a)
+# print(b)
+
+# print(a | b)
+
+# print(a & b)
+
+# print([1, 2, 3].union([4, 5, 6]))
+
